e9fed2913ef4d3aa9900fd5ecdc057e2a72bde05
q613307165-lang <q613307165@gmail.com>
2026-05-26 14:14:53 +0800

    Create rclone_tool.py

---
+++ app/tools/rclone_tool.py
@@ -0,0 +1,9 @@
+from typing import Any, Dict
+
+
+class RcloneTool:
+    """Placeholder for inspecting file sync jobs driven by rclone."""
+
+    async def summarize_jobs(self) -> Dict[str, Any]:
+        # TODO: parse rclone logs or run rclone commands
+        return {}
