1cc278110cb55c5890fc3add7adc0f0f4084bfb2
q613307165-lang <q613307165@gmail.com>
2026-05-26 14:14:40 +0800

    Create cloudflare_tool.py

---
+++ app/tools/cloudflare_tool.py
@@ -0,0 +1,9 @@
+from typing import Any, Dict
+
+
+class CloudflareTool:
+    """Placeholder for interacting with Cloudflare APIs (DNS, logs, firewall, etc.)."""
+
+    async def summarize_http_errors(self, zone_id: str) -> Dict[str, Any]:
+        # TODO: call Cloudflare Log APIs or Logpush destination
+        return {}
